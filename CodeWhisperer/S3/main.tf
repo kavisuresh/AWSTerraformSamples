8ba6f78bbf861ed6be56d11d3453d8c781649feb
Kavitha Suresh Kumar <kavi2002suresh@gmail.com>
2023-12-21 10:50:26 +0530

    Update main.tf

--- CodeWhisperer/S3/main.tf
+++ CodeWhisperer/S3/main.tf
@@ -1,21 +1,14 @@
-# create a s3 bucket with name demo237469236402
-resource "aws_s3_bucket" "demo237469236402" {
-    bucket = "demo237469236402"
-    tags = {
-        Name        = "demo237469236402"
-        Environment = "Dev"
-    }
-    }
+# create s3 bucket with name new7237492
+resource "aws_s3_bucket" "new7237492" {
+    bucket = "new7237492"
+  tags = {
+    Name = "new7237492"
+  }
+}
 
-
-# upload dog.jpg file to s3 bucket using s3 object resource
+# upload dog.jpg to s3 bucket using object resource
 resource "aws_s3_object" "dog" {
-    bucket = aws_s3_bucket.demo237469236402.id
-    key    = "dog.jpg"
-    source = "dog.jpg"
-    etag   = filemd5("dog.jpg")
-    tags = {
-        Name        = "dog.jpg"
-        Environment = "Dev"
-    }
-    } 
+  bucket = aws_s3_bucket.new7237492.id
+  key    = "dog.jpg"
+  source = "dog.jpg"
+}
